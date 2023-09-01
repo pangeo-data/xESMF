b9c1f150a233ce737a422618a36cf70280081337
charlesgauthier-udm <charlles.gauthier.1@umontreal.ca>
2023-09-01 11:11:19 -0400

    Moved the poly length check to its own function and modified the warning

--- xesmf/frontend.py
+++ xesmf/frontend.py
@@ -1184,20 +1184,8 @@ class SpatialAverager(BaseRegridder):
             self._lon_out_name = 'lon'
             self._lat_out_name = 'lat'
 
-        # Check length of polys segments, issue warning if too long
-        check_polys, check_holes, _, _ = split_polygons_and_holes(polys)
-        check_polys.extend(check_holes)
-        poly_segments = []
-        for check_poly in check_polys:
-            b = check_poly.boundary.coords
-            poly_segments.extend(
-                [LineString(b[k : k + 2]).length for k in range(len(b) - 1)]
-            )  # Length of each segment
-        if np.any(np.array(poly_segments) > 1.0):
-            warnings.warn(
-                'Polys contain large (>1deg) segments. This could lead to unsuspected errors. To prevent errors over large region, please densify your polys using the densify_poly function',
-                UserWarning,
-            )
+        # Check length of polys segments
+        self._check_polys_length(polys)
 
         poly_centers = [poly.centroid.xy for poly in polys]
         self._lon_out = np.asarray([c[0][0] for c in poly_centers])
@@ -1221,6 +1209,22 @@ class SpatialAverager(BaseRegridder):
             unmapped_to_nan=False,
         )
 
+    @staticmethod
+    def _check_polys_length(polys):
+        # Check length of polys segments, issue warning if too long
+        check_polys, check_holes, _, _ = split_polygons_and_holes(polys)
+        check_polys.extend(check_holes)
+        poly_segments = []
+        for check_poly in check_polys:
+            b = check_poly.boundary.coords
+            poly_segments.extend(
+                [LineString(b[k: k + 2]).length for k in range(len(b) - 1)]
+            )  # Length of each segment
+        if np.any(np.array(poly_segments) > 1.0):
+            warnings.warn(
+                'Polys contain large (>1deg) segments. This could lead to unsuspected errors. To prevent errors over large region, please segmentize your polys.',
+                UserWarning,
+            )
     def _compute_weights_and_area(self, mesh_out):
         """Return the weights and the area of the destination mesh cells."""
 
